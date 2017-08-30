d33df02ea96f78c10b190e9c151b69c0e1f3955c
ElRige <pierre.conord@gmail.com>
2017-08-30 15:54:30 +0200

    Create deploy.sh

---
+++ .circleci/deploy.sh
@@ -0,0 +1,12 @@
+cd /var/www/prod_deploy
+yarn install
+yarn build --prod 
+cp /var/www/prod/config.js ./
+cd /var/www
+mv ./prod ./prod_old
+mv ./prod_deploy ./prod
+cd prod
+pm2 stop prod
+pm2 start --name="prod"
+cd /var/www ; rm -rf prod_old
+echo "Well done !"
