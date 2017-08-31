5d69814c5d9a95d6d19a907b096c1fae83ed7880
Baptiste Foucher <contact@bfoucher.fr>
2017-08-31 11:43:21 +0200

    use local config for server port

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get('*', (req, res) => {
 /**
  * Get port from environment and store in Express.
  */
-const port = process.env.PORT || '3000';
+const port = config.express_port || '3000';
 app.set('port', port);
 
 /**
