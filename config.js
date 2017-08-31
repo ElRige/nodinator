a59b3643b122559ebbf99a9c13474aac93bffffd
Baptiste Foucher <contact@bfoucher.fr>
2017-08-31 11:41:23 +0200

    add config express port

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   "database_host": "localhost",
   "database_port": "27017",
   "database_name": "nodinator",
+  "express_port": "3000",
   "secret": "thisIsASecretPleaseChangeIt",
 };
